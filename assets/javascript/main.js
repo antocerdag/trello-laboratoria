406045d4c6631dc0049209a68fb073e30ec3b135
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-17 10:00:25 -0400

    se agrega boton y textarea, luego se agrega la tarea

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,45 @@
+document.getElementById("botoncito").addEventListener("click", 
 
-
-document.getElementById("boton").addEventListener("click",
-
-function botoncito(){
-	var tarea = document.getElementById("tarea").value;
-	document.getElementById("tarea").value = "";
-
-	var contenedor = document.getElementById("contenedor");
-
-	var nuevaTarea = document.createElement("div");
-	var textNuevaTarea = document.createTextNode(tarea);
+function newLista(){
+	var caja = document.getElementById("newCaja");
 	var nuevoContenedor = document.createElement("span");
+	nuevoContenedor.setAttribute("id", "newLista");
 
-	nuevoContenedor.appendChild(textNuevaTarea);
-	nuevaTarea.appendChild(nuevoContenedor);
-	contenedor.appendChild(nuevaTarea);
+	var nuevaTareaArea = document.createElement("textarea");
+	nuevaTareaArea.setAttribute("placeholder", "Añadir una lista");
+	nuevaTareaArea.setAttribute("id", "nuevaTareaArea");
 
-})
+	caja.appendChild(nuevaTareaArea);
+
+	var boton = document.createElement("button");
+	boton.setAttribute("type", "submit");
+	boton.setAttribute("id", "btn");
+
+	var botontxt = document.createTextNode("Añadir");
+
+	
+	caja.appendChild(boton);
+	boton.appendChild(botontxt);
+
+
+	document.getElementById("btn").addEventListener("click", 
+		function newTarea(){
+
+			var tarea = document.getElementById("nuevaTareaArea").value;
+			//document.getElementById("tarea").value = "";
+
+			var cont = document.getElementById("contenedor");
+
+			var nuevasTareas = document.createElement("div");
+			var textoNuevaTarea = document.createTextNode(tarea);
+			var elementoContenedor = document.createElement("span");
+
+			elementoContenedor.appendChild(textoNuevaTarea);
+			nuevasTareas.appendChild(elementoContenedor);
+			cont.appendChild(nuevasTareas);
+
+
+		})
+
+});
+
