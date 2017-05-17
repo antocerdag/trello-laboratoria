5898606ce0282794ac467d4439a8563dfdbbded9
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-17 09:00:06 -0400

    creando un contenedor donde se mostraran las tareas ingresadas

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,19 @@
-main.js
+
+
+document.getElementById("boton").addEventListener("click",
+
+function botoncito(){
+	var tarea = document.getElementById("tarea").value;
+	document.getElementById("tarea").value = "";
+
+	var contenedor = document.getElementById("contenedor");
+
+	var nuevaTarea = document.createElement("div");
+	var textNuevaTarea = document.createTextNode(tarea);
+	var nuevoContenedor = document.createElement("span");
+
+	nuevoContenedor.appendChild(textNuevaTarea);
+	nuevaTarea.appendChild(nuevoContenedor);
+	contenedor.appendChild(nuevaTarea);
+
+})
